6a7ad5d5eecabf886551a30e6c05d56c8b3e3a9c
Shewan Dagne <shewan.dagne@bluewin.ch>
2025-10-17 22:33:19 +0200

    Update app.py with author info

--- app.py
+++ app.py
@@ -195,6 +195,6 @@ if __name__ == "__main__":
     # Footer
     st.markdown("""
     <div class="footer">
-        © 2025 Book Recommendation System. All rights reserved. | <h4 style="color: blue;"> Developed by Shewan </h4>
+        © 2025 Book Recommendation System. All rights reserved. | <h6 style="color: blue;"> Developed by Shewan </h6>
     </div>
     """, unsafe_allow_html=True)
